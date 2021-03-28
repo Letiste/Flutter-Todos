dd5c8620bfbd6d6772932a1e33ee46cd0788744b
Léo Salé <leo.sale72@gmail.com>
2021-03-28 18:55:21 +0200

    add static home page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:todos/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('AppBar render a title', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    expect(find.text('Todos'), findsNWidgets(2));
+    expect(find.text('TodosApp'), findsOneWidget);
   });
 }
