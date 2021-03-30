bcfd2ac9baf493dd1505d18caa8b17093abbf4d5
Léo Salé <leo.sale72@gmail.com>
2021-03-30 09:09:04 +0200

    add create todo

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
-import 'todos.dart';
+import 'new_todo.dart';
+import 'todo.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Todo> todos = [];
+
+  void _addTodo() async {
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NewTodo()),
+    );
+    setState(() {
+      todos.add(Todo(text: result));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,10 +28,13 @@ class HomePage extends StatelessWidget {
         title: Text('TodosApp'),
         elevation: 8,
       ),
-      body: Todos(),
+      body: ListView(
+        padding: const EdgeInsets.fromLTRB(0, 16, 0, 64),
+        children: todos,
+      ),
       backgroundColor: Color.fromRGBO(245, 245, 245, 1),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _addTodo,
         child: Icon(
           Icons.add,
           size: 36,
